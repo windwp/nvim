c1d8d83713f63850189ac0ada5dd489f31a4a6c6
windwp <windwp@users.noreply.github.com>
2021-04-14 15:29:38 +0700

    update plug.lua

--- lua/wind/core/plug.lua
+++ lua/wind/core/plug.lua
@@ -13,59 +13,72 @@ local M={}
 
 -- customize paq
 M.Plug = function(opts)
-  local plugin = ''
-  if type(opts) == 'string' then
-    plugin = string.match(opts,'/(.*)')
-    opts = {opts}
-  else
-    plugin = string.match(opts[1],'/(.*)')
-  end
+    local plugin_name = ''
+    if type(opts) == 'string' then
+        plugin_name = string.match(opts,'/(.*)')
+        opts = {opts}
+    else
+        plugin_name = string.match(opts[1],'/(.*)')
+    end
 
-  if opts.on then
-    opts.opt = true
+    local add_to_rtp = true
+    if opts.on then
+        -- still have some problem here with argument on command
+        -- when you type it first time it will not understand argument
+        -- but it fine for me now
+        if opts.config then
+            vim.cmd(string.format(
+                [[command! -nargs=* -range -bang -complete=file %s silent! delcommand %s | packadd %s | call v:lua.Wind.load_plug('%s') | %s]],
+                opts.on, opts.on, plugin_name, opts.config, opts.on))
+            opts.config = false
+        else
+            vim.cmd(string.format(
+                [[command! -nargs=* -range -bang -complete=file %s silent! delcommand %s | packadd %s | %s]],
+                opts.on, opts.on, plugin_name, opts.on))
+        end
+        add_to_rtp = false
+    end
+
+    if opts.ft then
+        if opts.config then
+            vim.cmd(string.format(
+                [[au FileType %s ++once packadd %s | call v:lua.Wind.load_plug('%s') | e]] ,
+                opts.ft, plugin_name, opts.config))
+            opts.config = false
+        else
+            vim.cmd(string.format([[au FileType %s ++once packadd %s | e]] ,opts.ft,plugin_name))
+        end
+        add_to_rtp = false
+    end
+
+    if opts.cond ~= nil then
+        if opts.cond == true then
+            table.insert(packs, plugin_name)
+        else
+            opts.config = false
+            add_to_rtp = false
+        end
+    end
+
+    -- force all plugin is option except the opt = false
+    -- paq will add this to folder start but i don't like it because
+    -- if i remove the plugin from config file it still loading that
+    if opts.opt == nil then opts.opt = true end
+
+    if add_to_rtp == true then table.insert(packs, plugin_name) end
+
+    -- some config need to load before we use it
+    -- (vim global setting for vimscript)
+    if opts.before then
+        table.insert(configs_before, opts.before)
+    end
+
+    -- loading another config
     if opts.config then
-      vim.cmd(string.format([[command! -nargs=* -range -bang -complete=file %s silent! delcommand %s | packadd %s | call v:lua.Wind.load_plug('%s') | %s]], opts.on, opts.on, plugin, opts.config, opts.on))
-      opts.config = false
-    else
-      vim.cmd(string.format([[command! -nargs=* -range -bang -complete=file %s silent! delcommand %s | packadd %s | %s]], opts.on, opts.on, plugin, opts.on))
+        table.insert(configs,opts.config)
     end
-  end
 
-  if opts.ft then
-    opts.opt = true
-    if opts.config then
-      vim.cmd(string.format([[au FileType %s ++once packadd %s | call v:lua.Wind.load_plug('%s')]] , opts.ft, plugin, opts.config))
-      opts.config = false
-    else
-      vim.cmd(string.format([[au FileType %s ++once packadd %s]] ,opts.ft,plugin))
-    end
-  end
-
-  if opts.cond ~= nil then
-    opts.opt = true
-    if opts.cond == true then
-      table.insert(packs, plugin)
-    else
-    -- some config will nerver load here
-      opts.config = false
-    end
-  end
-
-  -- force all plugin is option except the opt = false
-  if opts.opt == nil then
-    opts.opt = true
-    table.insert(packs, plugin)
-  end
-
-  if opts.before then
-    table.insert(configs_before, opts.before)
-  end
-
-  if opts.config then
-    table.insert(configs,opts.config)
-  end
-
-  paq(opts)
+    paq(opts)
 
 end
 
@@ -76,12 +89,19 @@ M.load_config=function()
   end
 
   for _, plugin in pairs(packs) do
-    vim.cmd("packadd ".. plugin)
+    local ok, msg = pcall(vim.cmd, "packadd " .. plugin)
+    if not ok then
+      print('load pack error: ' .. plugin)
+      print(msg)
+    end
   end
 
   for _, value in pairs(configs) do
     Wind.load_plug(value)
+
   end
 end
+
+
 
 return M
